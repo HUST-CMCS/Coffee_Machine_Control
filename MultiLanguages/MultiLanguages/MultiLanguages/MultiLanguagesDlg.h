b623af0823ccfb4415c3c7ff69b9acb32083dda8
kunbb <464484836@qq.com>
2016-01-07 17:59:10 +0800

    zhf

---
+++ MultiLanguages/MultiLanguages/MultiLanguages/MultiLanguagesDlg.h
@@ -0,0 +1,38 @@
+
+// MultiLanguagesDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMultiLanguagesDlg 对话框
+class CMultiLanguagesDlg : public CDialogEx
+{
+// 构造
+public:
+	CMultiLanguagesDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_MULTILANGUAGES_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+public:
+	BOOL m_bRestartFlag;  // 重启对话框的标识
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnLanguageSwitch();
+	afx_msg void OnClose();
+};
