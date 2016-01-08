9372b7b63d70e15c2e20d109971bda37ae2d6789
carson0408 <940784559@qq.com>
2016-01-08 15:37:20 +0800

    changes

--- Frame.cpp
+++ Frame.cpp
@@ -1,27 +1,4 @@
-#ifndef FRAME
-#define FRAME
-/*
-class WorldFrame{
-	
-};
-
-class JointFrame{
-
-};
-*/
-
-class TaskFrame{                                            //工件坐标系
-	pravite:
-		double x;                                           //与世界坐标系偏移量x,y与旋转角度deg
-		double y;
-		double deg;
-	public:
-		TaskFrame(double a=0,double b=0,double d=0);         
-		void SetTaskFrame(double a,double b,double d);      //设置工件坐标系
-		double GetX();                                      //获取X方向偏移量x
-		double GetY();                                      //获取Y方向偏移量y
-		double GetDeg();                                    //获取旋转角度deg
-};
+#include"Frame.h"
 TaskFrame::TaskFrame(double a,double b,double d){
 	x=a;
 	y=b;
@@ -41,5 +18,3 @@ double TaskFrame::GetY(){
 double TaskFrame::GetDeg(){
 	return deg;
 }
-
-#endif
